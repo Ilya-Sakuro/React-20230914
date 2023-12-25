c7816c3a4a5b1d84c22e7bbb3db430fc15acf5fa
Ilya-Sakuro <114528785+Ilya-Sakuro@users.noreply.github.com>
2023-12-26 02:22:27 +0300

    fix: Slice return idle

--- src/redux/entities/restaurant/index.js
+++ src/redux/entities/restaurant/index.js
@@ -1,25 +1,16 @@
 import { createEntityAdapter } from '@reduxjs/toolkit';
 import { createSlice } from '@reduxjs/toolkit';
-import { REQUEST_STATUS } from '../../../constants/statusConfig';
 import { getRestaurants } from './thunks/get-restaurant';
 
 const entitiesAdapter = createEntityAdapter();
 
 const { reducer } = createSlice({
   name: 'restaurant',
-  initialState: entitiesAdapter.getInitialState({ status: REQUEST_STATUS.idle }),
+  initialState: entitiesAdapter.getInitialState(),
   extraReducers: (builder) =>
-    builder
-      .addCase(getRestaurants.pending, (state) => {
-        state.status = REQUEST_STATUS.pending;
-      })
-      .addCase(getRestaurants.fulfilled, (state, { payload }) => {
-        entitiesAdapter.setAll(state, payload);
-        state.status = REQUEST_STATUS.fulfilled;
-      })
-      .addCase(getRestaurants.rejected, (state) => {
-        state.status = REQUEST_STATUS.rejected;
-      }),
+    builder.addCase(getRestaurants.fulfilled, (state, { payload } = {}) => {
+      entitiesAdapter.setAll(state, payload);
+    }),
 });
 
 export default reducer;
