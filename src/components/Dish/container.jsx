1099b0915d12a91807e57f515f878319102f0e77
Ilya-Sakuro <114528785+Ilya-Sakuro@users.noreply.github.com>
2023-12-26 02:18:58 +0300

    feat: loading

--- src/components/Dish/container.jsx
+++ src/components/Dish/container.jsx
@@ -1,3 +1,4 @@
+import { SyncOutlined } from '@ant-design/icons';
 import { useSelector } from 'react-redux';
 import { selectDishById } from '../../redux/entities/dish/selectors';
 import { Dish } from './component';
@@ -6,7 +7,15 @@ export const DishContainer = ({ dishId }) => {
   const dish = useSelector((state) => selectDishById(state, dishId));
 
   if (!dish) {
-    return null;
+    return (
+      <SyncOutlined
+        style={{
+          fontSize: 24,
+          color: '#fa6400',
+        }}
+        spin
+      />
+    );
   }
 
   return <Dish dish={dish} />;
