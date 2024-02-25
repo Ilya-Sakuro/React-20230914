ebc0e2253d466305db25ed6dfc5a677f88efc241
Ilya-Sakuro <114528785+Ilya-Sakuro@users.noreply.github.com>
2024-02-25 17:25:49 +0300

    feat: basket button & drawer

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,11 +1,27 @@
+import { ShoppingCartOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
+import { Drawer } from 'antd';
+import { FloatButton } from 'antd';
 import classNames from 'classnames';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { ThemeContext } from '../../contexts/ThemeContext';
+import { selectDishAmountById } from '../../redux/UI/cart/selector';
 import { DishContainer } from '../Dish/container';
 import style from './style.module.scss';
 
 export const Menu = ({ menu }) => {
     const { theme } = useContext(ThemeContext);
+    const amount = useSelector(state => state?.cart);
+    const [open, setOpen] = useState(false);
+
+    const showDrawer = () => {
+        setOpen(true);
+    };
+
+    const onClose = () => {
+        setOpen(false);
+    };
 
     return (
         <>
@@ -22,6 +38,43 @@ export const Menu = ({ menu }) => {
                     </li>
                 ))}
             </ul>
+            <Drawer
+                zIndex={99999}
+                closable={false}
+                title='Cart'
+                onClose={onClose}
+                open={open}
+                placement={'left'}
+            >
+                {Object.keys(amount)?.map(id => (
+                    <li
+                        key={id}
+                        className={classNames(style.item, {
+                            [style.itemDark]: theme === 'dark',
+                        })}
+                    >
+                        <DishContainer dishId={id} />
+                    </li>
+                ))}
+                <Button
+                    type='primary'
+                    onClick={() => console.log(JSON.stringify({ Order: 'Success' }))}
+                >
+                    Send order
+                </Button>
+            </Drawer>
+            {Object.values(amount).find(e => e !== 0) && (
+                <FloatButton
+                    style={{ top: '13px', right: '84px', zIndex: 9999 }}
+                    badge={{
+                        count: Object.values(amount).reduce((pre, num) => {
+                            return pre + num;
+                        }),
+                    }}
+                    icon={<ShoppingCartOutlined />}
+                    onClick={showDrawer}
+                />
+            )}
         </>
     );
 };
